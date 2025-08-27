ac68628cea1abe0bc0602334b3f74ae1f1fdbd2d
Ikaros Bernardini <ikaros.bernardini@gmail.com>
2025-08-27 19:11:01 +0200

    Jobbat på ett inloggings script, har även ändrat namn på test.py till Ikaros.py för att använda som min test miljö i vårat repo.

--- test.py
+++ test.py
@@ -1,2 +1,17 @@
-print("Hello world!")
-
+print("Please input your username and password.")
+username = input('Username: ')
+password = input('Password: ')
+if username == 'Ikaros':
+    print('Hello, Ikaros')
+    if password == 'Telia123':
+        print('Access granted.')
+    else:
+        print('Wrong password.')
+        repeat = input('Try again? (yes/no): ')
+        if repeat.lower() == 'yes':
+            password = input('Enter password: ')
+            if password == 'Telia123':
+                print('Access granted.')
+            else:
+                print('Access denied.') 
+                
