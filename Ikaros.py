3a0be9b9111db0e73a88a473d030691f2ace6517
Ikaros Bernardini <ikaros.bernardini@gmail.com>
2025-08-27 21:37:20 +0200

    buggfix

--- Ikaros.py
+++ Ikaros.py
@@ -1,7 +1,7 @@
 korrekt_användarnamn = ("Ikaros") # korrect inloggnings användarnamn  
 korrekt_lösenord = ("Telia123") # korrect inloggnings lösenord
 
-def login_system(korrekt_användarnamn, korrekt_lösenord): # skapar en funktion för ett inloggningssystem
+def inloggnings_system(korrekt_användarnamn, korrekt_lösenord): # skapar en funktion för ett inloggningssystem
     felaktiga_försök = 3 # förbestämmer antal felaktiga försök som går att göra
     while felaktiga_försök > 0: # så länge felaktiga försök är större än 0
         användarnamn = input("Användarnamn:") # ber användaren skriva in sitt användarnamn
@@ -12,7 +12,7 @@ def login_system(korrekt_användarnamn, korrekt_lösenord): # skapar en funktion
         else:
             felaktiga_försök -= 1 # om användarnamn eller lösenord är felaktigt så minskar vi antalet försök med 1
             if felaktiga_försök == 0: # om felaktiga försök är 0 så skriver vi ut en text och avslutar programmet
-                print("För många felaktiga försök. Ditt program avslutas.")
+                print("För många felaktiga försök. Programmet avslutas.")
             else: # annars skriver vi ut en text med antal försök kvar och loopen fortsätter
-                print("Du har", felaktiga_försök, "försök kvar. Försök igen.") 
-login_system(korrekt_användarnamn, korrekt_lösenord) # kallar på funktionen
+                print("Felaktigt användarnamn eller lösenord. Du har", felaktiga_försök, "försök kvar, Försök igen.") 
+inloggnings_system(korrekt_användarnamn, korrekt_lösenord) # kallar på funktionen som vi skapade ovan. 
