f6f49511cd25605588e4ce5f070b9f5bafd78750
Ikaros Bernardini <ikaros.bernardini@gmail.com>
2025-08-27 17:31:53 +0200

    flyttade tillbaka filerna från ikaros-script till nackademin

---
+++ nackademin.py
@@ -0,0 +1,14 @@
+def greet():
+    print("Hello, welcome to Nackademin!")
+print("----------------")
+greet()  
+print('----------------')
+
+def Count():
+    for i in range(1, 11):
+        print(i)
+
+# kallar på funktion
+Count()
+greet()
+print("Hej Lowe!")
