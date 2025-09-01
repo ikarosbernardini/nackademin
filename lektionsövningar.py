f081d381c7dca9e7b8b841ae7cd433cf6e1fbe02
Ikaros Bernardini <ikaros.bernardini@gmail.com>
2025-09-01 13:57:17 +0200

    lagt in anteckningar från lektionen

--- lektionsövningar.py
+++ lektionsövningar.py
@@ -62,14 +62,80 @@
      #  hälsningsfras = hello("Ikaros")
      #  print(hälsningsfras)
 
-import random
-for i in range(100):  # Perform 100 coin flips.
-    if random.randint(0, 1) == 0:
-        print('H', end=' ')
-    else:
-        print('T', end=' ')
-print()  # Print one newline at the end.
+     #   import random
+     #   for i in range(100):  # Perform 100 coin flips.
+    #    if random.randint(0, 1) == 0:
+    #        print('H', end=' ')
+     #   else:
+    #        print('T', end=' ')
+    #    print()  # Print one newline at the end.
+
+#    animals: list = ["cat", "mouse", "cow", "dog", "horse"]
+##   for animal in animals:
+   #     print(f"{animal} at index {animals.index(animal)}")
+#
+
+   #   """"  animals = ["cat", "dog", "horse", "cat", "horse"]
+   #     new_animals = ""
+   #     for animal in animals:
+    #        new_animals = new_animals, animal
+   #         print(new_animals) """"
+
+#    """"animals = ["cat", "dog", "horse", "cat", "horse"]
+   # animals.append("mouse")
+#   animals.remove("cat")
+  #  print("cat" in animals)
+  #  print("house" in animals)
+
+    #    print("cat" not in animals)
+     #   print() """""""""""
+
+
+    #    animals = ["cat", "dog", "horse", ["dahlia", "daisy"]]
+
+    #    animals.append("mouse")
+    #    animals.remove("dog")
+     #   animals.insert(1, "spider")
+     #   animals[1] = "teddybear"
+
+# animals[3]
+
+# print(animals[3][0])
+
+    #    dagar = ["Mån","Tis","Ons","Tors","Fre"]
+     #   dagar.sort()
+     #   print(dagar)
+
+    #schema = [1,2,3],[1,2,3],[1,2,3],[1,2,3],[1,2,3]
+
+    #print(schema)
+
+     #   def matte(tal1, tal2):
+    #            return tal1+tal2, tal1-tal2, tal1/tal2
+
+     #   print(matte(23,32))
 
 
 
+   # def lista_djur():
+   #     return ["horse","cat","dog"], "Dennis"
+   # print(lista_djur()[0])
 
+     #   adress = {"namn":"Dennis","efternamn":"Rudin"}
+     #   print(adress)
+      #  print(list(adress.keys()))
+      #  print(adress.values())
+      #  print(adress["namn"])
+adress = [
+    {"namn":"Dennis","efternamn":"Rudin"},
+    {"namn":"Nina","efternamn":"Rudin"},
+    {"namn":"Juha","efternamn":"Rudin"},
+    {"namn":"Kalle","efternamn":"Rudin"}
+]
+
+print(adress[0].get("nisse"))
+
+for person in adress:
+    print("Namn:", person["namn"])
+    print("Efternamn:",person["efternamn"])
+   # print(person)
