cdc4d87bd8c28f5620a694580c3054c662bfab93
Ikaros Bernardini <ikaros.bernardini@gmail.com>
2025-09-10 14:15:16 +0200

    t

---
+++ Testmiljö-folder/matte.py
@@ -0,0 +1,35 @@
+# addera(tal1, tal2)
+
+def addera(tal1, tal2):
+    return tal1 + tal2 
+
+
+def subtrahera(tal1, tal2):
+    return tal1 - tal2
+
+
+def dividera(tal1, tal2):
+    if tal2 == 0:
+        return "Kan ej dividera med 0"
+    return tal2 / tal2
+    
+
+
+def multiplicera(tal1, tal2):
+    return tal1 * tal2
+
+
+
+
+def matte(tal1, tal2):
+    
+    print("Addera: ",str,addera(tal1, tal2))
+    print("Subtrahera: ",str,subtrahera(tal1, tal2))
+    print("Dividera: ", str,dividera(tal1, tal2))
+    print("Multiplicera: ", str,multiplicera(tal1, tal1))
+    matte()
+
+if __name__ == "__main__":
+    pass
+else:
+    print("__name__ = "+ __name__)
--- Testmiljö-folder/mymath.py
+++
@@ -1,8 +0,0 @@
-import matte
-import os 
-
-
-matte.
-
-if __name__ == "__main__":
-    matte(30, 39)
