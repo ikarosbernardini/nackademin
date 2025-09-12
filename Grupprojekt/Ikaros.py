627c2ff4e349d1c81d6156bdc8a3c1c623552c27
Ikaros Bernardini <ikaros.bernardini@gmail.com>
2025-09-12 12:29:20 +0200

    ändrade namn på mina metod så de blir i följd med importnamnet funk : t.ex. funk.spara

--- Grupprojekt/Ikaros.py
+++ Grupprojekt/Ikaros.py
@@ -2,11 +2,11 @@ import os
 
 if __name__ == "__main__": # fortfarnade jävligt oklart fan den denna kod gör, men den körs bara om filen körs direkt, "sägs de" 
     
-    def spara_funk(filnamn, uppgift):  # sparar uppgiften i en textfil
+    def spara(filnamn, uppgift):  # sparar uppgiften i en textfil
         with open(filnamn, 'w') as f: # öppnar filen i skrivläge
          f.write(uppgift) # skriver uppgiften i filen
 
-    def ladda_funk(filnamn): # laddar uppgiften från en textfil
+    def ladda(filnamn): # laddar uppgiften från en textfil
         if not os.path.exists(filnamn): # kollar om filen finns
             print(f"Filen '{filnamn}' hittades inte.") # om filen inte finns, skrivs detta ut
             return None
