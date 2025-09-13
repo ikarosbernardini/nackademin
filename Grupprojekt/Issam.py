79c3cc79294508f76a78e96b28681c975af84df2
issamhariri-code <issam@Pappa.localdomain>
2025-09-13 09:05:17 +0200

    Uppdaterade Issam.py med min del

--- Grupprojekt/Issam.py
+++ Grupprojekt/Issam.py
@@ -1,2 +1,29 @@
-print("Hello world")
+
+def lägg_till_uppgifter(uppgifter):
+    uppgift = input("Ange en uppgift att lägga till: ")
+    uppgifter.append("[]" + uppgift)
+    return uppgifter
+
+def lista_uppgifter(uppgifter):
+    if len(uppgifter) == 0:
+        print("Inga uppgifter i listan.")
+    else:
+        for uppgift in uppgifter:
+            print(uppgift)
+
+
+def markera_klar(uppgifter, index):
+    uppgifter[index] = "[X]" + uppgifter[index][4:]
+    return uppgifter
+
+def ta_bort_uppgift(uppgifter, index):
+    del uppgifter[index]
+    return uppgifter
+
+
+def todo_app():
+    uppgifter = []
+    uppgifter = lägg_till_uppgifter(uppgifter)
+    uppgifter = lägg_till_uppgifter(uppgifter)
+    lista_uppgifter(uppgifter)
 
