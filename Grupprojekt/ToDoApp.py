eeb612bfe5ffe9373d224c60c6ef13de755e83d9
Dolken01 <dolken001@hotmail.com>
2025-09-22 11:02:02 +0200

    t

--- Grupprojekt/ToDoApp.py
+++ Grupprojekt/ToDoApp.py
@@ -29,7 +29,7 @@ def meny(): #Skapar meny för användaren
                 exit()
             else: #Säger till om användaren inmatar något val som inte finns
                 print("Valet finns inte med i listan. Försök igen")
- # hej love, hur mår vi 
+ # hej love, hur mår vi. jag mår bra, själv då?
 meny()
 
 if __name__ == "__main__": 
