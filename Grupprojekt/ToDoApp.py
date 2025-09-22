37efbfb2f268592c4c2971e4cbb2e34d66336ca7
Ikaros Bernardini <ikaros.bernardini@gmail.com>
2025-09-22 11:03:57 +0200

    t

--- Grupprojekt/ToDoApp.py
+++ Grupprojekt/ToDoApp.py
@@ -1,6 +1,6 @@
 import os
 import Ikaros as funk # importerar Ikaros.py filen som "funk" för att kunna använda metoderna jag skapade.
-import Love as meny # importerar Love.py filen som "meny" för att kunna använda metoderna Love skapade. 
+import Love as m # importerar Love.py filen som "meny" för att kunna använda metoderna Love skapade. 
 import Issam as todo # importerar Issam.py filen som "todo" för att kunna använda metoderna Issam skapade.
 
 def meny(): #Skapar meny för användaren 
