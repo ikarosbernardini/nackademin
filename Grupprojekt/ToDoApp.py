43c9179091136e6f12a5cc61d2988bcdf686fc77
Ikaros Bernardini <ikaros.bernardini@gmail.com>
2025-09-22 10:51:23 +0200

    tja love jag ser detta

--- Grupprojekt/ToDoApp.py
+++ Grupprojekt/ToDoApp.py
@@ -29,7 +29,7 @@ def meny(): #Skapar meny för användaren
                 exit()
             else: #Säger till om användaren inmatar något val som inte finns
                 print("Valet finns inte med i listan. Försök igen")
- # hej 
+ # hej love
 meny()
 
 if __name__ == "__main__": 
